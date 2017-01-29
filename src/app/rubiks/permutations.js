4ffb4010789789a76d912fb65d1806f45366a293
Fernandez Ludovic <lfernandez.dev@gmail.com>
2017-01-29 21:50:54 +0100

    feat: better style and option 'all'.

--- src/app/rubiks/permutations.js
+++ src/app/rubiks/permutations.js
@@ -20,10 +20,19 @@ class PermutationsController {
 
     const vm = this;
     this.byCriteria = function (permutation) {
-      return permutation.white_face.layer === vm.criterias.whiteFace.layer &&
-        permutation.white_face.orientation === vm.criterias.whiteFace.orientation &&
-        permutation.edge.layer === vm.criterias.edge.layer &&
-        permutation.edge.position === vm.criterias.edge.position;
+      return (vm.criterias.whiteFace.layer === 'all' ||
+       permutation.white_face.layer === vm.criterias.whiteFace.layer) &&
+      (vm.criterias.whiteFace.orientation === 'all' ||
+       permutation.white_face.orientation === vm.criterias.whiteFace.orientation) &&
+      (vm.criterias.edge.layer === 'all' ||
+       permutation.edge.layer === vm.criterias.edge.layer) &&
+      (vm.criterias.edge.position === 'all' ||
+       permutation.edge.position === vm.criterias.edge.position);
+
+      // return permutation.white_face.layer === vm.criterias.whiteFace.layer &&
+      //   permutation.white_face.orientation === vm.criterias.whiteFace.orientation &&
+      //   permutation.edge.layer === vm.criterias.edge.layer &&
+      //   permutation.edge.position === vm.criterias.edge.position;
     };
   }
 }
